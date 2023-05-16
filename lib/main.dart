b2ea994ab9c18fcad828e069ef84bc5abe779deb
alviansaputrap <alviansaputra367@gmail.com>
2023-05-16 23:22:24 +0700

    utsflutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(belajarflut());
+void main() => runApp(BelajarFlutter());
 
-class belajarflut extends StatelessWidget {
+class BelajarFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Login"),
+          title: Text("YStore.id"),
+          backgroundColor: Color.fromARGB(255, 40, 100, 45), // Set the app bar color to blue
         ),
         body: Column(
           children: [
-            Text("Y Store"),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                  decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Username",
-              )),
+            Text(
+              "Login Dulu Cuy!",
+              style: TextStyle(
+                color: Color.fromARGB(255, 50, 50, 50), // Set the text color to red
+                fontSize: 18
+              ),
             ),
             Padding(
               padding: EdgeInsets.all(20),
               child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Password",
-                  )),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Username",
+                ),
+              ),
             ),
-            ElevatedButton(onPressed: () {}, child: Text("LOGIN"))
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Password",
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                  color: Colors.white, // Set the text color to white
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // Set the button background color to green
+              ),
+            )
           ],
         ),
       ),
